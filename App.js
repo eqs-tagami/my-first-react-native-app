4145666cb1157cb45f5b7f5c4def23e99b56c753
eqs-tagami <eqs.tagami@gmail.com>
2024-02-15 21:41:32 +0900

    3.8 コンポーネントを再利用する

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Welcome name="Bob" />
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
     </View>
   );
 }
