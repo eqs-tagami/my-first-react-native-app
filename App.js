79bd8a7ff3673440474ae16fe1d337fabc27b65c
eqs-tagami <eqs.tagami@gmail.com>
2024-02-15 21:12:25 +0900

    3.3 コンポーネントを使う

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome name="Bob" />
     </View>
+  );
+}
+
+function Welcome(props) {
+  return (
+    <Text>Hello, {props.name}</Text>
   );
 }
 
