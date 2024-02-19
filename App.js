ece5699e6b93fbcb3407236af1435c9f7774fb50
eqs-tagami <eqs.tagami@gmail.com>
2024-02-19 23:18:52 +0900

    5.3 allert

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    alert('hello!');
+  });
 
   return (
-    <View style={styles.container}>
-      <Button title="-1" onPress={ () => setCount(count - 1)} />
-      <Text>{count}</Text>
-      <Button title="+1" onPress={ () => setCount(count + 1)} />
+    <View>
+      <Text>Hello!</Text>
     </View>
   );
 };
